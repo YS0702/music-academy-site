bab77806284cc13ae49c0025665bdec1f3de47ea
Yash Srivastava <srivastavay711@gmail.com>
2025-08-25 18:39:04 +0530

    framer-motion bug fix

--- src/components/ui/navbar-menu.tsx
+++ src/components/ui/navbar-menu.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
-import { motion } from "motion/react";
+import { motion, Transition } from "framer-motion";
 
 
 
-const transition = {
+const transition: Transition = {
   type: "spring",
   mass: 0.5,
   damping: 11.5,
